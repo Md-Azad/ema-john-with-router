9af089d80f477cc94f50f5c601062de67fbac414
Md-Azad <sokalazad@gmail.com>
2023-04-08 23:25:05 +0200

    added styles on cards

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,14 +6,17 @@ const Product = (props) => {
   return (
     <div className="product">
       <img src={img} alt="" />
-      <h6>{name}</h6>
-      <p>Price:${price}</p>
-      <p>
-        <small>Manufacturer:{seller}</small>
-      </p>
-      <p>
-        <small>Ratings:{ratings} stars</small>
-      </p>
+      <div className="product-info">
+        <h6>{name}</h6>
+        <p>Price:${price}</p>
+        <p>
+          <small>Manufacturer:{seller}</small>
+        </p>
+        <p>
+          <small>Ratings:{ratings} stars</small>
+        </p>
+      </div>
+      <button className="btn-cart">Add to cart</button>
     </div>
   );
 };
