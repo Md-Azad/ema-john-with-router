9e82d19ce5a1abfb7e60bb66618d69232d553611
Md-Azad <sokalazad@gmail.com>
2023-04-08 23:55:01 +0200

    added handleAddToCart fuction and fav icon

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,22 +4,33 @@ import Product from "../Product/Product";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
         ))}
       </div>
 
       <div className="cart-container">
         <h4>order summary</h4>
+        <h3>total order: {cart.length}</h3>
       </div>
     </div>
   );
